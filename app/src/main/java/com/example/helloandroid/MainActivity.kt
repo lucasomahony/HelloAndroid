9dad2dc95b0cdefbb8407b96b0d44ba90efbd3cc
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-02 21:19:24 +0000

    Simplifying the onClickListener syntax V1

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -5,17 +5,9 @@ import android.os.Bundle
 import android.view.View
 import com.example.helloandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(){
     lateinit var binding: ActivityMainBinding
 
-    override fun onClick(v: View?) {
-        if (v?.equals(binding.btnGreet) == true) {
-            binding.tvGreeting.text = getString(R.string.button_pressed_message)
-        }
-        if (v?.equals(binding.button2) == true) {
-            binding.tvGreeting.text = getString(R.string.button2_pressed_message)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +15,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val view = binding.root
         setContentView(view)
 
-        binding.btnGreet.setOnClickListener(this)
+        binding.btnGreet.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?){
+                if (v?.equals(binding.btnGreet) == true) {
+                    binding.tvGreeting.text = getString(R.string.button_pressed_message)
+                }
+            }
+        })
 
-        binding.btnGreet.setOnClickListener(this) // how do I do this?
+        binding.btnGreet.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?){
+                if (v?.equals(binding.button2) == true) {
+                    binding.tvGreeting.text = getString(R.string.button2_pressed_message)
+                }
+            }
+        })
 
     }
 }
+
