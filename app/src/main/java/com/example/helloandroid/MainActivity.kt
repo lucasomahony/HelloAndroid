2a07076a78bea774cd7327cd153e9e2e77778c42
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-02 20:52:06 +0000

    make the button do something

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.helloandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityMainBinding
+
+    override fun onClick(v: View?) {
+        binding.tvGreeting.text = getString(R.string.button_pressed_message)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnGreet.setOnClickListener(this)
+
     }
 }
